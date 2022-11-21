6469f11f65fc47ea7518359b2c549231dbb53215
tomastu <tuweiwen@live.com>
2022-11-21 23:33:26 +0800

    temp commit for github preview

--- app/src/main/java/com/example/weatherdemo/WeatherApplication.kt
+++ app/src/main/java/com/example/weatherdemo/WeatherApplication.kt
@@ -3,16 +3,25 @@ package com.example.weatherdemo
 import android.annotation.SuppressLint
 import android.app.Application
 import android.content.Context
+import android.content.res.AssetManager
+import java.io.BufferedReader
+import java.io.InputStreamReader
 
 class WeatherApplication : Application() {
     companion object {
         @SuppressLint("StaticFieldLeak")
         lateinit var context: Context
+        lateinit var noConstTOKEN: String
         const val TOKEN = "qRAzNzgluiHbsTnJ"
+
+//        var ad = BufferedReader(InputStreamReader(context.assets.open("token"))).readText()
     }
 
     override fun onCreate() {
         super.onCreate()
         context = applicationContext
+//        context = baseContext
+
+        noConstTOKEN = BufferedReader(InputStreamReader(assets.open("token"))).readText()
     }
 }
